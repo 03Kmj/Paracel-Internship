e03e26bc24e061bff1a99e18cdcec9d92c48a59c
강민정 <zjql0403@knu.ac.kr>
2025-01-02 09:47:42 +0700

    fix : indexerror 수정

--- Assesment/pb2.py
+++ Assesment/pb2.py
@@ -2,19 +2,35 @@ def sieve(limit):
     if limit < 2:
         return []
     
-    if limit == 2:
-        return [False, True]
-
-    if limit == 3:
-        return [False, True, True]
-
-    res = [False]
-
+    primes = [False] * (limit + 1)
+    
     if limit >= 2:
-        res[2] = True
+        primes[2] = True
 
     if limit >= 3:
-        res[3] = True
+        primes[3] = True
+        
+    for x in range(1, int(limit**0.5) +1):
+        for y in range(1, int(limit**0.5)+1):
+            n = 4 * x**2 + y**2
+            if n <= limit and (n % 12 == 1 or n % 12 ==5):
+                primes[n] = not primes[n]
+                
+            n = 3 * x**2 + y**2
+            if n <= limit and n % 12 == 7:
+                primes[n] = not primes[n]
+                
+            n= 3 * x**2 - y**2
+            if x > y and n <= limit and n % 12 == 11:
+                primes[n] = not primes[n]
+    
+    for n in range(5, int(limit**0.5)+1):
+        if primes[n]:
+            for k in range(n**2, limit+1, n**2):
+                primes[k] = False
+                
+    primes_list = [n for n in range(limit +1) if primes[n]]
+    return primes_list
 
     for i in range(4, limit + 1):
         res[i] = False
@@ -69,7 +85,6 @@ def hash(string, modulus):
 
 
 if __name__ == '__main__':
-
     # generate primes list to use as modulus
     primes = sieve(10000) # modify limit based on your needs
     modulus = pick_prime(primes, 1000)
