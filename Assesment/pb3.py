e8bbce712de5a67daed9737f29e60896f167da4f
강민정 <zjql0403@knu.ac.kr>
2025-01-02 10:53:10 +0700

    fix : pb3 해결

--- Assesment/pb3.py
+++ Assesment/pb3.py
@@ -10,7 +10,8 @@ is_draw = None
 
 
 WIDTH = 400
-HEIGHT = 400 # Add 100 later for the 
+HEIGHT = 500 # Add 100 later for the 
+GRID_SIZE = 400
 BACKGROUND = (255, 255, 255)
 LINE_COLOR = (0, 0, 0)
 
@@ -20,35 +21,29 @@ grid = [[None]*3, [None]*3, [None]*3]
 pg.init()
 FPS = 30
 clock = pg.time.Clock()
-screen = pg.display.set_mode((WIDTH, HEIGHT + 100), 0, 32) # 400 x 500 display
+
+screen = pg.display.set_mode((WIDTH, HEIGHT), 0, 32) # 400 x 500 display
 pg.display.set_caption("Tic Tac Toe")
-size = 80 # size of the X / O marks
+
+font = pg.font.Font(None, 30)
+size = GRID_SIZE // 6 - 20 # size of the X / O marks
 
 def game_initiating_window():
     """initializes game window"""
-    pg.display.update()
-    time.sleep(3)
     screen.fill(BACKGROUND)
-    pg.draw.line(screen, LINE_COLOR, (WIDTH / 3, 0), (WIDTH / 3, HEIGHT), 7)
-    pg.draw.line(screen, LINE_COLOR, (WIDTH / 3 * 2, 0), (WIDTH / 3 * 2, HEIGHT), 7)
-    pg.draw.line(screen, LINE_COLOR, (0, HEIGHT / 3), (WIDTH, HEIGHT / 3), 7)
-    pg.draw.line(screen, LINE_COLOR, (0, HEIGHT / 3 * 2), (WIDTH, HEIGHT / 3 * 2), 7)
+    for i in range (1,3):
+        pg.draw.line(screen, LINE_COLOR, (i * GRID_SIZE // 3, 0), (i * GRID_SIZE // 3, GRID_SIZE), 7)
+        pg.draw.line(screen, LINE_COLOR, (0, i * GRID_SIZE // 3), (GRID_SIZE, i * GRID_SIZE // 3), 7)
     draw_status()
 
 def draw_status():
     """draws the status bar"""
-    global is_draw
-    if current_winner is None:
-        message = current_player.upper() + "'s Turn"
-    else:
-        message = current_winner.upper() + " won !"
-    if is_draw:
-        message = "Game Draw !"
-    font = pg.font.Font(None, 30)
-    text = font.render(message, 1, (255, 0, 0))
-    screen.fill((0, 0, 0), (0, 400, 475, 100))
-    text_rect = text.get_rect(center=(WIDTH / 2, HEIGHT/2))
-    screen.blit(text, text_rect)
+    global is_draw, current_winner
+    status_message = f"{current_player}'s Turn" if not current_winner and not is_draw else \
+        (f"{current_winner} Wins!" if current_winner else "It's a Draw!")
+    status_loca = font.render(status_message, True, LINE_COLOR)
+    screen.fill(BACKGROUND, (0, GRID_SIZE, WIDTH, HEIGHT - GRID_SIZE))
+    screen.blit(status_loca, (WIDTH // 2 - status_loca.get_width() // 2, GRID_SIZE + 25))
     pg.display.update()
 
 
@@ -58,26 +53,26 @@ def check_win():
     for row in range(0, 3):
         if (grid[row][0] == grid[row][1] == grid[row][2]) and (grid[row][0] is not None):
             current_winner = grid[row][0]
-            pg.draw.line(screen, (250, 0, 0),
-                         (0, (row + 1)*HEIGHT / 3 - HEIGHT / 6),
-                         (WIDTH, (row + 1)*HEIGHT / 3 - HEIGHT / 6), 4)
+            pg.draw.line(screen, (255, 0, 0),
+                         (0, (row + 0.5) * GRID_SIZE // 3),
+                         (GRID_SIZE, (row + 0.5) * GRID_SIZE // 3), 7)
             break
         
     for col in range(0, 3):
         if (grid[0][col] == grid[1][col] == grid[2][col]) and (grid[0][col] is not None):
             current_winner = grid[0][col]
-            pg.draw.line(screen, (250, 0, 0),
-                         ((col + 1) * WIDTH / 3 - WIDTH / 6, 0),
-                         ((col + 1) * WIDTH / 3 - WIDTH / 6, HEIGHT), 4)
+            pg.draw.line(screen, (255, 0, 0),
+                         ((col + 0.5) * GRID_SIZE // 3, 0),
+                         ((col + 0.5) * GRID_SIZE // 3, GRID_SIZE), 7)
             break
 
     if (grid[0][0] == grid[1][1] == grid[2][2]) and (grid[0][0] is not None):
         current_winner = grid[0][0]
-        pg.draw.line(screen, (250, 70, 70), (50, 50), (350, 350), 4)
+        pg.draw.line(screen, (255, 0, 0), (0, 0), (GRID_SIZE, GRID_SIZE), 7)
         
     if (grid[0][2] == grid[1][1] == grid[2][0]) and (grid[0][2] is not None):
         current_winner = grid[0][2]
-        pg.draw.line(screen, (250, 70, 70), (350, 50), (50, 350), 4)
+        pg.draw.line(screen, (255, 0, 0), (GRID_SIZE, 0), (0, GRID_SIZE), 7)
 
     if (all([all(row) for row in grid]) and current_winner is None):
         is_draw = True
@@ -88,64 +83,26 @@ def check_win():
 def drawXO(row, col):
     """draws the X's and O's on the board"""
     global grid, current_player
-    if row == 1:
-        pos_x = 50
-
-    if row == 2:
-        pos_x = WIDTH / 3 + 30
-
-    if row == 3:
-        pos_x = WIDTH / 3 * 2 + 30
-
-    if col == 1:
-        pos_y = 25
-
-    if col == 2:
-        pos_y = HEIGHT / 3 + 30
-
-    if col == 3:
-        pos_y = HEIGHT / 3 * 2 + 30
-
-    grid[row-1][col-1] = current_player
+    x, y = col * GRID_SIZE // 3 + GRID_SIZE // 6, row * GRID_SIZE // 3 + GRID_SIZE // 6
+    
     if current_player == 'x':
-        pg.draw.line(screen, (0, 0, 0), (pos_y, pos_x), (pos_y + size, pos_x + size), 5)
-        pg.draw.line(screen, (0, 0, 0), (pos_y, pos_x + size), (pos_y + size, pos_x), 5)
-        current_player = 'o'
-
+        pg.draw.line(screen, LINE_COLOR, (x - size, y - size), (x + size, y + size), 5)
+        pg.draw.line(screen, LINE_COLOR, (x - size, y + size), (x + size, y - size), 5)
+    
     else:
-        center = (pos_y + size // 2, pos_x + size // 2)
-        radius = size // 2 - 5
-        pg.draw.circle(screen, (0, 0, 0), center, radius, 5)
-        current_player = 'x'
+        pg.draw.circle(screen, LINE_COLOR, (x,y), size, 5)
+
+    grid[row][col] = current_player
+    current_player = 'O' if current_player == 'X' else 'X'
     pg.display.update()
 
 
 
 def user_click():
     """finds the position of the user's click"""
-    x, y = pg.mouse.get_pos()
-    if (x < WIDTH / 3):
-        col = 1
-    elif (x < WIDTH / 3 * 2):
-        col = 2
-    elif (x < WIDTH):
-        col = 3
-    else:
-        col = None
-        
-    if (y < HEIGHT / 3):
-        row = 1
-
-    elif (y < HEIGHT / 3 * 2):
-        row = 2
-
-    elif (y < HEIGHT):
-        row = 3
-
-    else:
-        row = None
-
-    if (row and col and grid[row-1][col-1] is None):
+    x,y = pg.mouse.get_pos()
+    row, col = y // (GRID_SIZE // 3), x // (GRID_SIZE // 3)
+    if (row < 3 and col < 3 and grid[row][col] is None):
         drawXO(row, col)
         check_win()
 
@@ -154,27 +111,25 @@ def user_click():
 def reset_game():
     """restarts game on win or draw"""
     global grid, current_winner, current_player, is_draw
-    time.sleep(3)
+    time.sleep(10)
     current_player = 'x'
-    is_draw = False
-    game_initiating_window()
     current_winner = None
+    is_draw = False
     grid = [[None]*3, [None]*3, [None]*3]
+    game_initiating_window()
 
 game_initiating_window()
 
 while True: # continue loop until user closes the window
     for event in pg.event.get(): # check for new events
-        if event.type == QUIT:
+        if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
-        elif event.type == MOUSEBUTTONDOWN:
+        elif event.type == pg.MOUSEBUTTONDOWN:
             user_click()
             if (current_winner or is_draw):
                 reset_game()
-    pg.display.update()
-
-    clock.tick(FPS)
+clock.tick(FPS)
 
 
 
